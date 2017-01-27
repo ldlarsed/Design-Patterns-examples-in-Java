4287e5f6751b1221add73c5b84ee99552ed653f5
Lukas Larsed <git@lukaslarsed.com>
2017-01-27 15:00:33 +0100

    DAO - classes

---
+++ DAO Classes/src/mvc/controller/Controller.java
@@ -0,0 +1,41 @@
+package mvc.controller;
+
+import java.sql.SQLException;
+
+import mvc.model.Model;
+import mvc.model.Person;
+import mvc.model.PersonDAO;
+import mvc.view.LoginFormEvent;
+import mvc.view.LoginListener;
+import mvc.view.View;
+
+/**
+ * Listener class that listens to fired events.
+ *
+ */
+public class Controller implements LoginListener{
+	
+	private View view;
+	private Model model;
+	
+	private PersonDAO personDAO = new PersonDAO();
+	
+	public Controller(Model model, View view) {
+		this.view = view;
+		this.model = model;
+	}
+	
+	@Override
+	public void userCreated(LoginFormEvent event) {
+		System.out.println("\nLogin event received: " + event.getName() + " " + event.getPassword());
+		
+		try {
+			personDAO.addPerson(new Person(event.getName(), event.getPassword()));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
---
+++ DAO Classes/src/mvc/model/PersonDAO.java
@@ -0,0 +1,44 @@
+package mvc.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * One DAO class per table or view. 
+ * CRUD functionality must be implemented.
+ *
+ */
+public class PersonDAO {
+
+	public void addPerson(Person person) throws SQLException{
+		Connection conn = Database.getInstance().getConnection(); //usnig singleton
+		
+		PreparedStatement p = conn.prepareStatement("insert into people (name, password) values (?,?)");
+	
+		p.setString(1, person.getName());
+		p.setString(2, person.getPassword());
+		
+		System.out.println("\nExecuting database updadte " + person.getName() + " " + person.getPassword());
+		
+		p.executeUpdate();
+		p.close();
+	}
+	
+	public Person getPerson(int id){
+		return null;
+	}
+	
+	public List<Person> getPeople(){
+		return null;
+	}
+	
+	public void updatePerson(Person person){
+		
+	}
+	
+	public void deletePerson(int id){
+		
+	}
+}
